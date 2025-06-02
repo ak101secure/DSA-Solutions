fdcb5f16a53e7819a718747c1e83a9da70cd8f10
afzal <afzal@gmail.com>
2025-06-02 23:08:09 +0600

    file and code chnage

---
+++ Array/output.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int array[5] = {1,3,43,45,5};
+
+    for(int i = 0;i<5;i++){
+        cout<<"index is "<<i<<"value of this is :"<<array[i]<<endl;
+    }
+
+    return 0;
+}
